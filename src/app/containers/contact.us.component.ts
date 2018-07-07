7805ebbec5a89f97e19d1db101eedf233834d3d5
shubho9999 <36084874+shubho9999@users.noreply.github.com>
2018-07-07 13:58:00 +0530

    edit login,contact,signup,notfound

--- src/app/containers/contact.us.component.ts
+++ src/app/containers/contact.us.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,29 +7,48 @@ import {Component} from '@angular/core';
     <div>
       <img style="width: 106%; height: 100%" src="../../assets/images/contact-us.jpeg"/>
       <h1>Contact Us</h1>
-      <p>Our Company is here to provide you more information,answer any questions you may have <br> and create an effective
-        solution for your need</p>
+      <p class="company-text">Our Company is here to provide you more information,answer any questions you may have <br>
+        and create an effective solution for your need</p>
       <mat-icon class="animated fadeInDown infinite time" (click)=scrollToBottom()>arrow_drop_down_circle</mat-icon>
     </div>
-    <mat-card style="opacity: 0.7">
-      <mat-card class="contact-card">
-        <mat-card-header>
-          Contact Us
-        </mat-card-header>
-        <mat-card-content fxLayout="column">
-          <div>
-            <mat-form-field style="margin-top: 8%;">
-              <input matInput placeholder="Email">
-            </mat-form-field>
+    <div class="u-margin-bottom-small-1"></div>
+    <div class="book">
+      <div class="book__form">
+        <form class="form" (submit)="onSend()" [formGroup]="loginForm">
+          <div class="u-margin-bottom-small">
+            <h2 class="heading-secondary" style="margin-left: 50px;">
+              CONTACT US
+            </h2>
           </div>
-          <div>
-            <mat-form-field style="margin-top: 8%;">
-              <input matInput placeholder="Name">
-            </mat-form-field>
+          <div class="blog">
+            <p class="hire">Need help? Want to hire me as a freelancer for your work?<br>Feel free to contact
+              me and I would be happy to help!</p>
+            <span class="account">Email: <a class="footer__link" routerLink="#"> shubham99varshney@gmail.com</a></span>
           </div>
-        </mat-card-content>
-      </mat-card>
-    </mat-card>
+
+          <div class="example-container" fxLayout="column" fxLayoutGap="18px">
+            <mat-form-field [color]="accent">
+              <input matInput placeholder="Full Name" [formControl]="fullNameControl">
+              <mat-error *ngIf="fullNameControl.invalid">{{getErrorMessageFullName()}}</mat-error>
+            </mat-form-field>
+
+            <mat-form-field [color]="accent">
+              <input matInput placeholder="Email" [formControl]="emailControl">
+              <mat-error *ngIf="emailControl.invalid">{{getErrorMessageEmail()}}</mat-error>
+            </mat-form-field>
+
+            <mat-form-field [color]="accent">
+              <textarea matInput placeholder="Message(Optional)" [formControl]="messageControl"></textarea>
+            </mat-form-field>
+
+            <mat-card-actions>
+              <button mat-raised-button color="accent">SEND</button>
+            </mat-card-actions>
+          </div>
+        </form>
+      </div>
+    </div>
+
   `,
   styles: [`
     h1 {
@@ -40,10 +60,10 @@ import {Component} from '@angular/core';
       text-shadow: 0 0 30px rgba(0, 0, 0, .25);
       background-image: linear-gradient(to right bottom, #e79f13, #ff6803);
       -webkit-background-clip: text;
-      color:transparent;
+      color: transparent;
     }
 
-    p {
+    .company-text {
       bottom: 38%;
       position: absolute;
       align-items: center;
@@ -53,6 +73,12 @@ import {Component} from '@angular/core';
       font-size: 1.5em;
       text-shadow: 0 0 30px rgba(0, 0, 0, .5);
       text-align: center;
+    }
+    .hire {
+      font-size: 17px;
+      margin-left: 6px;
+      color: #757575;
+      margin-top: 5px;
     }
 
     mat-icon {
@@ -64,33 +90,93 @@ import {Component} from '@angular/core';
       left: 48%;
     }
 
-    mat-card {
-      height: 100%;
-      width: 102.3%;
-      margin-top: -3px;
-    }
-
-    .contact-card {
-      width: 50%;
-      height: 90%;
-      margin-left: 17%;
-      margin-top: 3%;
-    }
-
     mat-icon:hover {
       opacity: 1;
       transition: all 1s;
     }
 
-    mat-card-header {
-      margin-left: 26%;
-      font-size: 2.5em;
-      color: #ff8505;
-      letter-spacing: 11px;
+    .mat-card-actions .mat-button:first-child, .mat-card-actions .mat-raised-button:first-child {
+      margin-left: 100px;
+      margin-bottom: 20px;
     }
 
-    mat-form-field {
-      width: 40%;
+    .u-margin-bottom-small-1 {
+      margin-bottom: 1.5rem;
+    }
+
+    .book {
+      background-image: linear-gradient(105deg, rgba(255, 255, 255, 0.9) 0%, rgba(255, 255, 255, 0.9) 50%, transparent 50%),
+      url(../../assets/images/contact.jpg);
+      background-size: 100%;
+      border-radius: 3px;
+      box-shadow: 1px 1px 20px 15px rgba(0, 0, 0, 0.2);
+      height: 100%;
+      width: 900px;
+      margin-left: 220px;
+      margin-top: 0px;
+    }
+
+    .u-margin-bottom-small {
+      margin-bottom: 0.6rem;
+    }
+
+    .heading-secondary {
+      font-size: 3rem;
+      text-transform: uppercase;
+      font-weight: 700;
+      display: inline-block;
+      background-image: linear-gradient(to right, #e79f13, #ff6803);
+      -webkit-background-clip: text;
+      color: transparent;
+      letter-spacing: .2rem;
+      transition: all .2s;
+    }
+    .heading-secondary:hover {
+      transform: skewY(2deg) skewX(15deg) scale(1.1);
+      text-shadow: 0.5rem 1rem 2rem rgba(0, 0, 0, 0.2);
+    }
+
+    .blog {
+      margin-left: 30px;
+      margin-bottom: 30px;
+      display: block;
+      background-color: #F9F9F9;
+      width: 410px;
+      padding: 4px 4px;
+      border-radius: 4px;
+      box-shadow: 1px 1px 20px 5px rgba(0, 0, 0, 0.2);
+
+    }
+
+    .account {
+      color: #757575;
+      margin-left: 35px;
+      
+    }
+
+    .example-container {
+      margin-left: 50px;
+    }
+
+    .example-container > * {
+      width: 300px;
+    }
+
+    .footer__link:link, .footer__link:visited {
+      color: #ff6803;
+      background-color: rgba(255, 255, 255, 0);
+      text-decoration: none;
+      text-transform: uppercase;
+      display: inline-block;
+      cursor: pointer;
+      transition: all .2s;
+    }
+
+    .footer__link:hover, .footer__link:active {
+      color: #fff;
+      background-color: #e79f13;
+      box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.4);
+      transform: rotate(5deg) scale(1.3);
     }
 
     .time {
@@ -99,7 +185,33 @@ import {Component} from '@angular/core';
   `]
 })
 export class ContactUsComponent {
+  fullNameControl = new FormControl('', [Validators.required]);
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  messageControl = new FormControl('');
+  loginForm: FormGroup;
+
   constructor() {
+    this.loginForm = new FormGroup({
+      myFullname: this.fullNameControl,
+      myEmail: this.emailControl,
+      myMessage: this.messageControl
+    });
+  }
+
+  getErrorMessageFullName() {
+    return this.fullNameControl.hasError('required') ? 'Fullname is required' : '';
+  }
+
+  getErrorMessageEmail() {
+    return this.emailControl.hasError('required') ? 'Email is required' :
+      this.emailControl.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+
+
+  onSend() {
+    console.log('form submitted', this.fullNameControl.value, this.emailControl.value, this.messageControl);
+    this.loginForm.reset();
   }
 
   scrollToBottom() {
--- src/app/containers/login.component.ts
+++ src/app/containers/login.component.ts
@@ -3,10 +3,197 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-login',
   template: `
-    <p>This is a Login page</p>
+    <form>
+      <input  checked id='signin' name='action' type='radio' value='signin'>
+      <label  for='signin'>login</label>
+      <input  id='signup' name='action' type='radio' value='signup'>
+      <label  for='signup'>Sign up</label>
+      <input  id='reset' name='action' type='radio' value='reset'>
+      <label  for='reset'>Reset</label>
+      <div id='wrapper'>
+        <div id='arrow'></div>
+        <input id='email' placeholder='Email' type='text'>
+        <input id='pass' placeholder='Password' type='password'>
+        <input id='mob' placeholder='Mobile number' type='text'>
+        <input id='repass' placeholder='confirm password' type='password'>
+      </div>
+      <!--<button  type='submit'><span>Reset password</span><br></button>-->
+      <!---->
+      <!--<button  type='submit'><span>Sign in</span><br></button>-->
+      <!---->
+      <!--<button  type='submit'><span>Sign up</span><br></button>-->
+      <button>
+    <span>
+      Reset password
+      <br>
+      login
+      <br>
+      Sign up
+    </span>
+      </button>
+      </form>
   `,
   styles: [`
+    @import url(https://fonts.googleapis.com/css?family=Raleway:700,800);
 
+    :focus {
+      outline: none;
+    }
+
+    ::-webkit-input-placeholder {
+    color: #DEDFDF;
+    }
+
+    ::-moz-placeholder {
+    color: #DEDFDF;
+    }
+
+    :-moz-placeholder {
+      color: #DEDFDF;
+    }
+
+    ::-ms-input-placeholder {
+    color: #DEDFDF;
+    }   
+
+    #wrapper, label, #arrow, button  {
+      transition: all .5s cubic-bezier(.6, 0, .4, 1);
+    }
+    
+    #wrapper {
+      overflow: hidden;
+    }
+    
+    #signin:checked ~ #wrapper {
+      height: 134px;
+    }
+    
+    #signin:checked ~ #wrapper #arrow {
+      left: 32px;
+    }
+    
+    #signin:checked ~ button span {
+      transform: translate3d(0, -72px, 0);
+    }
+    
+    #signup:checked ~ #wrapper {
+      height: 258px;
+    }
+    
+    #signup:checked ~ #wrapper #arrow {
+      left: 137px;
+    }
+    
+    #signup:checked ~ button span {
+      transform: translate3d(0, -144px, 0);
+    }
+    
+    #reset:checked ~ #wrapper {
+      height: 72px;
+    }
+    
+    #reset:checked ~ #wrapper #arrow {
+      left: 404px;
+    }
+    
+    #reset:checked ~ button span {
+      transform: translate3d(0, 0, 0);
+    }
+
+    form {
+      width: 450px;
+      height: 370px;
+      margin: -185px -225px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+    }
+
+    input[type=radio] {
+      display: none;
+    }
+
+    label {
+      cursor: pointer;
+      color: #ff8505;
+      display: inline-block;
+      font-size: 22px;
+      font-weight: 800;
+      opacity: .5;
+      margin-bottom: 30px;
+      text-transform: uppercase;
+    }
+
+    label:hover {
+      transition: all .3s cubic-bezier(.6, 0, .4, 1);
+      opacity: 1;
+      color: #ffab00;
+    }
+
+    label[for="signin"] {
+      margin-right: 20px;
+    }
+
+    label[for="reset"] {
+      float: right;
+    }
+
+    input[type=radio]:checked + label {
+      opacity: 1;
+    }
+
+    input[type=text],
+    input[type=password] {
+      background: #fff;
+      color: #ff8505;
+      border: none;
+      border-radius: 8px;
+      font-size: 20px;
+      font-family: 'Raleway', sans-serif;
+      height: 50px;
+      width: 99.5%;
+      margin-bottom: 10px;
+      opacity: 1;
+      text-indent: 20px;
+      transition: all .2s ease-in-out;
+    }
+
+    button {
+      background: #ff8505;
+      border: none;
+      border-radius: 8px;
+      color: #fff;
+      cursor: pointer;
+      font-family: 'Raleway', sans-serif;
+      font-size: 27px;
+      height: 50px;
+      width: 100%;
+      margin-bottom: 10px;
+      overflow: hidden;
+      transition: all .3s cubic-bezier(.6, 0, .4, 1);
+    }
+
+    button span {
+      display: block;
+      line-height: 72px;
+      position: relative;
+      top: -12px;
+      transform: translate3d(0, 0, 0);
+    }
+
+    button:hover {
+      background: #ffab00;
+    }
+
+    #arrow {
+      height: 0;
+      width: 0;
+      border-bottom: 10px solid #fff;
+      border-left: 10px solid transparent;
+      border-right: 10px solid transparent;
+      position: relative;
+      left: 32px;
+    }
   `]
 })
 export class LoginComponent {
