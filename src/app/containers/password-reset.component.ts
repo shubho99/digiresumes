ea0b2b896377e6c522b83a9685a0708855a046bf
shubho9999 <36084874+shubho9999@users.noreply.github.com>
2018-08-14 19:13:52 +0530

    temp commit

--- src/app/containers/password-reset.component.ts
+++ src/app/containers/password-reset.component.ts
@@ -7,28 +7,27 @@ import {AlertService} from '../modules/core/services/alert.service';
 @Component({
   selector: 'app-password-reset',
   template: `
-    <div  fxLayoutAlign="center center" class="overlay">
+    <div fxLayoutAlign="center center" class="overlay">
       <div fxLayout="column" fxLayoutGap="10px">
-        <mat-card fxFlex="100%" style="background-color: #000000">
+        <mat-card fxFlex="100%" style="background-color: #000000; box-shadow: inset 0px 0px 20px 10px rgba(0, 0, 0, 0.2);">
           <form [formGroup]="passwordResetForm" (submit)="passwordResetForm.valid && resetPassword()">
             <div fxLayout="row" fxLayoutGap="20px" fxLayoutAlign="start center">
-              <h1>Reset Password</h1>
+              <h1 class="form-text">Reset Password Form</h1>
               <span fxFlex="1 1 auto"></span>
             </div>
-            <mat-form-field style="width: 100%;margin-top: 2%">
+            <mat-form-field class="password-text" style="margin-top: 5%;">
               <input matInput placeholder="New Password" [formControlName]="'new_password'" type="password">
               <mat-hint>Password must be of atleast 8 digits</mat-hint>
               <mat-error>New Password is required</mat-error>
             </mat-form-field>
-            <mat-form-field style="width: 100%">
+            <mat-form-field class="password-text" style="margin-top: 1.5%;">
               <input matInput type="password" [formControlName]="'confirm_password'" placeholder="Confirm Password">
-              <mat-hint>Password and confirm password should match</mat-hint>
+              <mat-hint>Password and confirm password should be match</mat-hint>
               <mat-error> Confirm Password is required</mat-error>
             </mat-form-field>
-            <div fxLayout="row" fxLayoutAlign="end center">
-              <button type="submit" style="margin-top: 7%" mat-raised-button fxFlexAlign="end" color="primary"
-                      color="accent">
-                Submit
+            <div fxLayout="row" fxLayoutAlign="center center">
+              <button color="primary" type="submit" class="reset-button" mat-raised-button>
+                Reset my password
               </button>
             </div>
           </form>
@@ -39,14 +38,45 @@ import {AlertService} from '../modules/core/services/alert.service';
   `,
   styles: [`
     .overlay {
-      background: #ffdab4;
-      background-image: -webkit-radial-gradient(top, circle cover, #ffbea6, #ff7d04 80%);
-      background-image: -moz-radial-gradient(top, circle cover, #ffbea6, #ff7d04 80%);
-      background-image: -o-radial-gradient(top, circle cover, #ffbea6, #ff7d04 80%);
-      background-image: radial-gradient(top, circle cover, #ffbea6, #ff7d04 80%);
+      /*background: #d4f7c8;*/
+      /*background-image: -webkit-radial-gradient(top, circle cover, #16CB99, #3FC54B 90%);*/
+      /*background-image: -moz-radial-gradient(top, circle cover, #d4f7c8, #3fc54b 90%);*/
+      /*background-image: -o-radial-gradient(top, circle cover, #d4f7c8, #3FC54B 90%);*/
+      /*background-image: radial-gradient(top, circle cover, #d4f7c8, #3FC54B 90%);*/
+      /*background-image: linear-gradient(to right bottom, #3FC54B, #16CB99);*/
       min-height: 100%;
       min-width: 100%;
     }
+
+    .form-text {
+      text-transform: uppercase;
+      background-image: linear-gradient(to right bottom, #ffab00, #ff8505);
+      -webkit-background-clip: text;
+      color: transparent;
+      font-size: 32px;
+      margin-left: .6em;
+    }
+
+    .password-text {
+      width: 81%;
+      margin-left: 9.5%;
+    }
+
+    .reset-button {
+      margin-top: 10%;
+      margin-bottom: 2%;
+      color: whitesmoke;
+      /*background: #16CB99;*/
+      text-transform: uppercase;
+    }
+
+    /*::ng-deep .mat-form-field-ripple {*/
+    /*background-color: #16CB99 !important;*/
+    /**/
+    /*}*/
+    /*::ng-deep .mat-input-element {*/
+    /*color: #16CB99 !important;*/
+    /*}*/
   `]
 })
 export class PasswordResetComponent {
