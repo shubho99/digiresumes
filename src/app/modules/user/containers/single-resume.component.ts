622bfc8a4319965d246241c70063e1a505227422
shubho9999 <36084874+shubho9999@users.noreply.github.com>
2018-08-29 08:02:26 +0530

    designing of digital resume

--- src/app/modules/user/containers/single-resume.component.ts
+++ src/app/modules/user/containers/single-resume.component.ts
@@ -8,60 +8,342 @@ import {Resume} from '../../core/models/resume';
   template: `
     <div fxLayout="row">
       <mat-card class="side-bar-card">
+        <div fxLayout="column" fxLayoutGap="30px">
+          <img style="width: 100%" src="../../../../../assets/images/testimonial.png"/>
+          <h2 fxLayoutAlign="center start">
+            {{this.resume.contact_details.first_name}} {{this.resume.contact_details.last_name}}
+          </h2>
+          <div fxLayout="row" fxLayoutGap="20px" fxLayoutAlign="center start">
+            <button mat-mini-fab>
+              <mat-icon (click)="editResume()" style="font-size: 25px" matTooltip="edit-resume">assignment</mat-icon>
+            </button>
+            <button mat-mini-fab>
+              <mat-icon (click)="editProfile()" style="font-size: 25px" matTooltip="edit-profile video or picture">
+                videocam
+              </mat-icon>
+            </button>
+            <button mat-mini-fab>
+              <mat-icon style="font-size: 25px" matTooltip="share">share</mat-icon>
+            </button>
+          </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 10%">
+              <mat-icon style="font-size: 25px">account_circle</mat-icon>
+              <p class="contact-summary container-1">{{this.resume.contact_details.summary}}</p>
+            </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 5%">
+              <mat-icon style="font-size: 25px">call</mat-icon>
+              <p class="contact-summary">{{this.resume.contact_details.phone_number}}</p>
+            </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 5%">
+              <mat-icon style="font-size: 25px">email</mat-icon>
+              <p class="contact-summary container-1">{{this.resume.contact_details.email}}</p>
+            </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 5%">
+              <mat-icon style="font-size: 25px">home</mat-icon>
+              <p class="contact-summary container-1">
+                {{this.resume.contact_details.address}}, <br>
+                {{this.resume.contact_details.city}}, {{this.resume.contact_details.state}} -
+                {{this.resume.contact_details.zip_code}}
+                <br> {{this.resume.contact_details.country}}
+              </p>
+            </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 5%">
+              <mat-icon style="font-size: 25px">calendar_today</mat-icon>
+              <div fxFlex="column" class="container-1">
+                <div *ngFor="let skills of this.resume['skills']">
+                  <h4 style="color: #fff; margin-bottom: 5%">Professional skills</h4>
+                  <p class="contact-summary">{{skills.skill}}</p>
+                  <mat-progress-bar mode="determinate" value="70"></mat-progress-bar>
+                </div>
+              </div>
+            </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 5%">
+              <mat-icon style="font-size: 25px">calendar_today</mat-icon>
+              <div fxFlex="column" class="container-1">
+                <div *ngFor="let languages of this.resume['languages']">
+                  <h4 style="color: #fff; margin-bottom: 5%">Languages</h4>
+                  <p class="contact-summary">{{languages.name}}</p>
+                  <mat-progress-bar mode="determinate" value="70"></mat-progress-bar>
+                </div>
+              </div>
+            </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 5%">
+              <mat-icon style="font-size: 25px">favorite</mat-icon>
+              <div fxFlex="column" class="container-1">
+                <h4 style="color: #fff; margin-bottom: 5%">Strengths</h4>
+                <div *ngFor="let strengths of this.resume['strengths']">                 
+                  <ul style="color: #fff;">
+                    <li>
+                      {{strengths.name}}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div fxLayout="row" fxLayoutGap="50px" style="color: #fff; margin-top: 5%">
+              <mat-icon style="font-size: 25px">broken_image</mat-icon>
+              <div fxFlex="column" class="container-1">
+                <h4 style="color: #fff; margin-bottom: 5%">Weakness</h4>
+                <div *ngFor="let weakness of this.resume['weakness']">                  
+                  <ul style="color: #fff;">
+                    <li>
+                      {{weakness.name}}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </div>
       </mat-card>
       <div fxLayout="column">
         <mat-card fxLayout="column">
           <mat-card-header>
             <div fxLayout="row" fxLayoutGap="50px" class="username">
               <mat-icon>book</mat-icon>
-              <span>Education</span>
+              <p>Education</p>
             </div>
           </mat-card-header>
           <mat-card-content>
             <div fxLayout="column" class="education-card" fxLayoutGap="20px">
-              <div class="container" fxFlex="column">
-                <div *ngFor="let resume of this.resume['interests']">{{resume.interest}}</div>
-                <p>Firebase gives you functionality like analytics, databases, messaging
-                  and crash reporting so you can move quickly and focus on your users</p>
-              </div>
               <div fxFlex="column">
-                <h1>Build Digital Resume <br>And share your link</h1>
-                <p>Firebase gives you functionality like analytics, databases, messaging
-                  and crash reporting so you can move quickly and focus on your users</p>
-              </div>
-              <div fxFlex="column">
-                <h1>Build Digital Resume <br>And share your link</h1>
-                <p>Firebase gives you functionality like analytics, databases, messaging
-                  and crash reporting so you can move quickly and focus on your users</p>
+                <div style="line-height: 20pt" *ngFor="let education of this.resume['education']">
+                  <h3>{{education.degree_type}}
+                    <span style="text-transform: lowercase !important; font-size: 10pt;">@{{education.school_name}}</span></h3>
+                  <span style="color: #5da4d9">{{education.graduation_month}} {{education.graduation_year}}</span>
+                  <h4 class="container">
+                    {{education.city}}, {{education.state}} <br>
+                    {{education.field}} - {{education.percentage}}
+                  </h4>
+                </div>
               </div>
             </div>
           </mat-card-content>
         </mat-card>
+
         <mat-card fxLayout="column">
+          <mat-card-header>
+            <div fxLayout="row" fxLayoutGap="50px" class="username">
+              <mat-icon>book</mat-icon>
+              <p>Experience</p>
+            </div>
+          </mat-card-header>
+          <mat-card-content>
+            <div fxLayout="column" class="education-card" fxLayoutGap="20px">
+              <div fxFlex="column">
+                <div style="line-height: 20pt" *ngFor="let employmentHistory of this.resume['employment_history']">
+                  <h3>{{employmentHistory.designation}}
+                    <span style="text-transform: lowercase !important; font-size: 10pt;">
+                  @{{employmentHistory.organisation}}
+                  </span>
+                  </h3>
+                  <span style="color: #5da4d9" *ngIf="employmentHistory.end_month">
+                    {{employmentHistory.start_month}} {{employmentHistory.start_year}} - 
+                    {{employmentHistory.end_month}} {{employmentHistory.end_year}}
+                  </span>
+                  <span style="color: #5da4d9" *ngIf="!employmentHistory.end_month">
+                    {{employmentHistory.start_month}} {{employmentHistory.start_year}}
+                  </span>
+                  <h4 class="container">
+                    {{employmentHistory.city}}, {{employmentHistory.state}} <br>
+                    {{employmentHistory.employer}}
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </mat-card-content>
+        </mat-card>
+
+        <mat-card fxLayout="column">
+          <mat-card-header>
+            <div fxLayout="row" fxLayoutGap="50px" class="username">
+              <mat-icon>book</mat-icon>
+              <p>Hobbies and interest</p>
+            </div>
+          </mat-card-header>
+          <mat-card-content style="margin: 5%;">
+            <div fxLayout="column" class="education-card" fxLayoutGap="20px">
+              <div fxFlex="column">
+                <div style="line-height: 20pt;" *ngFor="let interests of this.resume['interests']">
+                  <ul>
+                    <li>
+                      {{interests.interest}}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </mat-card-content>
+        </mat-card>
+
+        <mat-card fxLayout="column">
+          <mat-card-header>
+            <div fxLayout="row" fxLayoutGap="50px" class="username">
+              <mat-icon>book</mat-icon>
+              <p>industrial exposure</p>
+            </div>
+          </mat-card-header>
+          <mat-card-content>
+            <div fxLayout="column" class="education-card" fxLayoutGap="20px">
+              <div fxFlex="column">
+                <div style="line-height: 20pt" *ngFor="let industrialExposures of this.resume['industrialExposures']">
+                  <h3>{{industrialExposures.organisation}}</h3>
+                  <span style="color: #5da4d9" *ngIf="industrialExposures.end_month">
+                    {{industrialExposures.start_month}} {{industrialExposures.start_year}} - 
+                    {{industrialExposures.end_month}} {{industrialExposures.end_year}}
+                  </span>
+                  <span style="color: #5da4d9" *ngIf="!industrialExposures.end_month">
+                    {{industrialExposures.start_month}} {{industrialExposures.start_year}}
+                  </span>
+                  <h4>
+                    {{industrialExposures.city}}, {{industrialExposures.state}}
+                  </h4>
+                  <p class="container">{{industrialExposures.work}}</p>
+                </div>
+              </div>
+            </div>
+          </mat-card-content>
+        </mat-card>
+
+        <mat-card fxLayout="column">
+          <mat-card-header>
+            <div fxLayout="row" fxLayoutGap="50px" class="username">
+              <mat-icon>book</mat-icon>
+              <p>awards and achievement</p>
+            </div>
+          </mat-card-header>
+          <mat-card-content style="margin: 5%;">
+            <div fxLayout="column" class="education-card" fxLayoutGap="20px">
+              <div fxFlex="column">
+                <div style="line-height: 20pt;" *ngFor="let award_achivements of this.resume['award_achivements']">
+                  <ul>
+                    <li>
+                      {{award_achivements.awards_and_achivements}}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </mat-card-content>
+        </mat-card>
+
+        <mat-card fxLayout="column">
+          <mat-card-header>
+            <div fxLayout="row" fxLayoutGap="50px" class="username">
+              <mat-icon>book</mat-icon>
+              <p>objective</p>
+            </div>
+          </mat-card-header>
+          <mat-card-content>
+            <div fxLayout="column" class="education-card" fxLayoutGap="20px">
+              <div fxFlex="column">
+                <div style="line-height: 20pt" *ngFor="let objectives of this.resume['objectives']">
+                  <h3>
+                    {{objectives.objective}}<br>
+                    {{objectives.date}}<br>
+                    {{objectives.place}}
+                  </h3>
+                  <p>{{objectives.declaration}}</p>
+                </div>
+              </div>
+            </div>
+          </mat-card-content>
+        </mat-card>
+
+        <mat-card fxLayout="column">
+          <mat-card-header>
+            <div fxLayout="row" fxLayoutGap="50px" class="username">
+              <mat-icon>book</mat-icon>
+              <p>reference</p>
+            </div>
+          </mat-card-header>
+          <mat-card-content>
+            <div fxLayout="column" class="education-card" fxLayoutGap="20px">
+              <div fxFlex="column">
+                <div style="line-height: 20pt" *ngFor="let refrences of this.resume['refrences']">
+                  <h3>
+                    {{refrences.name}}<br>
+                    {{refrences.relationship}}<br>
+                    {{refrences.company}}<br>
+                    {{refrences.email}}<br>
+                    {{refrences.phone}}<br>
+                    {{refrences.address}}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </mat-card-content>
+        </mat-card>
+
+        <mat-card fxLayout="column">
+          <mat-card-header>
+            <div fxLayout="row" fxLayoutGap="50px" class="username">
+              <mat-icon>book</mat-icon>
+              <p>project details</p>
+            </div>
+          </mat-card-header>
+          <mat-card-content>
+            <div fxLayout="column" class="education-card" fxLayoutGap="20px">
+              <div fxFlex="column">
+                <div style="line-height: 20pt" *ngFor="let projectDetails of this.resume['projectDetails']">
+                  <h3>
+                    {{projectDetails.title}}<br>
+                    {{projectDetails.description}}<br>
+                    {{projectDetails.duration}}<br>
+                    {{projectDetails.role}}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </mat-card-content>
         </mat-card>
       </div>
-    </div>
-    <div style="margin-top: 1%; margin-left: 1%" class="alternate" fxLayout="row" fxLayoutGap="20px">
-      <button (click)="editResume()" mat-raised-button color="primary">Edit Resume</button>
-      <button (click)="editProfile()" mat-raised-button color="accent">Edit profile Video or Picture</button>
     </div>
     <ngx-loading [show]="loading"></ngx-loading>
   `,
   styles: [`
+    .contact-summary {
+      color: #fff;
+      font-weight: 300;
+      font-size: 10pt;
+      line-height: 17pt;
+    }
+
+    h3, span {
+      text-transform: uppercase;
+      color: #767270;
+      margin-top: 5%;
+    }
+
+    h4, p {
+      color: #767270;
+    }
+
+    h2 {
+      color: #fff;
+      font-weight: bold;
+      text-transform: uppercase;
+      line-height: 37pt;
+    }
+
+    ul {
+      color: #767270;
+      font-weight: bold;
+      font-size: 16px;
+    }
+
     .side-bar-card {
-      background: #eaf1f8;
+      background: #538EC3;
       box-shadow: 0 3px 1px -2px rgba(0, 0, 0, .2), 0 2px 2px 0 rgba(0, 0, 0, .14), 0 1px 5px 0 rgba(0, 0, 0, .12);
-      width: 25%;
+      width: 30%;
       margin-left: 15%;
-      margin-top: 2.4%;
     }
 
     mat-card {
       background: #eaf1f8;
       box-shadow: 0 3px 1px -2px rgba(0, 0, 0, .2), 0 2px 2px 0 rgba(0, 0, 0, .14), 0 1px 5px 0 rgba(0, 0, 0, .12);
-      width: 75%;
+      width: 80%;
       margin-left: 2.5%;
-      margin-top: 3.7%;
+      margin-top: 35px;
     }
 
     mat-icon {
@@ -76,13 +358,18 @@ import {Resume} from '../../core/models/resume';
     }
 
     .education-card {
-      margin-top: 5% !important;
       margin-left: 15%;
     }
 
     .container {
-      padding-bottom: 5%;
+      padding-bottom: 4%;
       border-bottom: 1px solid #767270;
+    }
+
+    .container-1 {
+      padding-bottom: 10%;
+      border-bottom: 1px solid #fff;
+      margin-bottom: 15%;
     }
   `]
 })
