1d9cc7c5b896dab9f2ae536ec0d21c9d017248bd
shagungarg <shagungarg2010@gmail.com>
2018-09-29 16:51:28 +0530

    added test add

--- src/app/containers/home.component.ts
+++ src/app/containers/home.component.ts
@@ -24,7 +24,7 @@ import {Router} from '@angular/router';
       </button>
     </div>
     <app-footer [isHomeResponsive]="true" footerMargin="22%"></app-footer>  
-    
+    <ng-adsense adClient="ca-pub-3514316608905290" adSlot="6859632339" adFormat="auto"  ></ng-adsense>
   `,
   styles: [`
     #spin {
